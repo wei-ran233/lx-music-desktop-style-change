7457bf5c792fc9858af60e5941eed43c1f762b95
wei-ran233 <1814267825@qq.com>
2026-07-24 17:04:14 +0800

    build: disable npmRebuild in electron-builder config for binary packaging

--- build-config/build-pack.js
+++ build-config/build-pack.js
@@ -11,6 +11,7 @@ const afterPack = require('./build-after-pack')
 const options = {
   appId: 'cn.toside.music.desktop',
   productName: 'lx-music-desktop',
+  npmRebuild: false,
   beforePack,
   afterPack,
   protocols: {
